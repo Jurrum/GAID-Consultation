0337ce804824cdc86449946aa006ad0d16f5ac7f
Jurrum <84908345+Jurrum@users.noreply.github.com>
2024-10-22 08:45:42 +0200

    Added Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-// frontend/src/App.js
-
+import logo from './logo.svg';
+import './App.css';
 import React from 'react';
 import ChatbotComponent from './chatbot/ChatbotComponent';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Pre-Consultation Chatbot</h1>
-      <ChatbotComponent />
+      <header className="App-header">
+       
+        <h1>Pre-Consultation Chatbot</h1>
+        <ChatbotComponent />
+       
+        
+      </header>
+      <body>
+        
+      </body>
     </div>
   );
 }
